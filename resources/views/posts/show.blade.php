001586e5254333d12696d26321ae709d542c41b3
vikash2806 <vikashrajendran2806@gmail.com>
2020-11-15 19:51:15 +0530

    show updated

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,32 +2,31 @@
 @extends('layouts.crud')
 
 @section('title')
-    POSTS
+POSTS
 @endsection
 
 @section('content')
 <a href="/lsapp/public/posts" class="btn btn-outline-dark">Go Back</a>
 <br>
-<div>           
-     <br>
-<h3>{{$post->title}}</h3>
+<div>
+    <br>
+    <h3>{{$post->title}}</h3>
     <medium>{{$post->body}}</medium>
 </div>
 <hr>
-<small>{{$post->created_at}}</small> 
+<small>{{$post->created_at}}</small>
 
 <hr>
 <br>
 <a href="/lsapp/public/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
 
 <form action="{{ action('PostController@destroy',$post->id) }}" method="post">
-@method('DELETE')
-@csrf
- <button type="submit" class="btn btn-danger float-sm-right">DELETE</button>
+    @method('DELETE')
+    @csrf
+    <button type="submit" class="btn btn-danger float-sm-right">DELETE</button>
 </form>
 
-{{-- {{$post->title}}
-{{$post->body}} --}}
+
 
 @endsection
 
